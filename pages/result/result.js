0b5f88ef8b0a0c96b73227de3d94033166eaec92
liuhui <liuhuidut@sina.com>
2020-12-04 16:41:00 +0800

    增加雷达图及分组评价显示

--- pages/result/result.js
+++ pages/result/result.js
@@ -6,6 +6,18 @@ Page({
    */
   data: {
     current_tab: 'result',
+    evaluations: [],
+    aspectCanvas: {
+      canvasWidth: 392,
+      canvasHeight: 366,
+      fontSize: 40,
+      categories: ['1', '2', '3', '4', '5', '6'],
+      MaxDimension:6
+    },
+    currentPetNumber: 'm000225',
+    dimension: [0, 0, 0, 0, 0, 0],
+    radarData: {},
+    currentRadarImage: ""
   },
   handleTabChange(event) {
     if (event.detail.key === "homepage") {
@@ -14,11 +26,39 @@ Page({
       })
     }
   },
+  returnRadarData(dimension) {
+    const petNumber = this.data.currentPetNumber;
+    return {
+        gender: 1,
+        id: petNumber,
+        radarList: dimension
+    };
+  },
+  radarUpdate(e) {
+      //传回来雷达图片
+      this.setData({currentRadarImage: e.detail});
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    try {
+      const result = wx.getStorageSync('result')
+      console.log('result', result);
+      
+      if (result) {
+        let aspectCanvas = this.data.aspectCanvas;
+        let dimension = this.data.dimension;
+        let evaluations = result.group_level;
 
+        aspectCanvas.categories = result.group.titles;
+        aspectCanvas.MaxDimension = result.group.levels.length;
+        dimension = result.group.levels;
+        this.setData({aspectCanvas, dimension, evaluations});
+      }
+    } catch (e) {
+      // Do something when catch error
+    }
   },
 
   /**
@@ -32,7 +72,12 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    setTimeout(()=>{
+      const radarData = this.returnRadarData(this.data.dimension);
+      this.setData({radarData});
+      const {gender, id, radarList} = this.data.radarData;
+      console.log(gender, id, radarList);
+    },500);
   },
 
   /**
